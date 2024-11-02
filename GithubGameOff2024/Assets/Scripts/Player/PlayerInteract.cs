ee744accae3db75afe970cf6b1ffc0ef77e8690f
MacDudeDude <alexbrhls@gmail.com>
2024-11-02 14:14:35 -0700

    Added basic interaction
    Added an interaction interface
    Added player interaction using a sphere trigger collider

---
+++ GithubGameOff2024/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private Transform interactIcon;
+
+    private IInteractable interactable;
+
+    private void Start()
+    {
+        interactIcon.parent = null;
+    }
+
+    private void Update()
+    {
+        if(InputManager.Instance.WasInteractButtonPressed())
+        {
+            if(interactable != null)
+            {
+                interactable.Interact();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IInteractable newInteract;
+        if(other.TryGetComponent(out newInteract))
+        {
+            interactable = newInteract;
+            interactIcon.gameObject.SetActive(true);
+            interactIcon.transform.position = other.gameObject.transform.position + Vector3.up * 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IInteractable newInteract;
+        if (other.TryGetComponent(out newInteract))
+        {
+            if(newInteract == interactable)
+            {
+                interactable = null;
+                interactIcon.gameObject.SetActive(false);
+            }
+        }
+    }
+}
