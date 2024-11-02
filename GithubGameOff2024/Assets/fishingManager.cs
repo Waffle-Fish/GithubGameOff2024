b932fd3e057e533001072befca2967ba8d820fe8
Brandon Martin Del Campo <brandondevprog@gmail.com>
2024-11-02 10:49:19 -0700

    testScript

---
+++ GithubGameOff2024/Assets/fishingManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class fishingManager : MonoBehaviour
+{
+   
+    
+    public float FishReel;
+
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
